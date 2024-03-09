3f884f92f89f3b6fe8f36f612cf7c6dcc26196c3
DevJeremias <143229911+DevJeremias@users.noreply.github.com>
2024-03-09 16:48:58 -0300

    aprofundando os estudos

---
+++ exercicios/ex007.js
@@ -0,0 +1,6 @@
+var vel = 55.7
+console.log(`a velocidade do seu veiculo é de ${vel}Km/h.`)
+if (vel > 50) { //condição simples
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+}
+console.log(`Dirija com cuidado!`)
